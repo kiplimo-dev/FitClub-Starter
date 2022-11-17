859a43a6fe924fbb08c32b2be0dd26e4a0125dc8
kiplimo-dev <kiplimocollins855@gmail.com>
2022-11-17 14:23:21 +0300

    added more files

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+const Hero = () => {
+  return (
+    <div className="hero">
+       <div className="left-h"></div>
+    <div className='right-h'></div>
+    </div>
+  )
+} 
+
+export default Hero
